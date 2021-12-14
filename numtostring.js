0dbe987ee6524985a8513d596f7f16c039202b07
Phani54321 <92142841+Phani54321@users.noreply.github.com>
2021-12-14 18:31:52 +0530

    m

---
+++ numtostring.js
@@ -0,0 +1,23 @@
+var num=10;
+console.log(typeof(num));
+
+var num=10;
+console.log(num.toString());
+console.log(typeof(num.toString));
+
+var num='1';
+console.log(typeof(num));
+console.log(parseInt(num));
+console.log(typeof(parseInt(num)));
+
+var strNum="10.568";
+console.log(strNum);
+console.log(typeof(strNum));
+console.log(parseFloat(strNum));
+
+var num="10.23567";
+console.log(num);
+console.log(num.toFixed);
+console.log(typeof(num));
+
+
